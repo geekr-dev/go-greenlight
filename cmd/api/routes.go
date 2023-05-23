a358a938d0ba51abc137f9433b1e81196cc75c04
geekr <geekr@hey.com>
2023-05-23 17:33:51 +0800

    generate authentication tokens

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,6 +23,7 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHanlder)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	return app.recoverPanic(app.rateLimit(router))
 }
